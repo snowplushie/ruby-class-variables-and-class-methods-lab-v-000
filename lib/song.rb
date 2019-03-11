ac94abe21c2a4885a5c0494b4d4d8bd9f905a2aa
Paige Wiener <snowplushie@yahoo.com>
2019-03-11 02:07:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,11 @@ class Song
   @@artists = []
   @@genres = []
   
-  def initialize(name)
+  def initialize(name, artist, genre)
     @name = name
+    @artist = artist
+    @genre = genre
+    @count += 1
+    
   end
 end
