a8528c7401b2acd186505c06a7a901e60aa2d9b3
Paige Wiener <snowplushie@yahoo.com>
2019-03-11 02:15:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,11 +30,13 @@ class Song
   def self.genre_count
     genre_hash = Hash.new(0)
     @@genres.each {|genre| genre_hash[genre] += 1}
+    genre_hash
   end
   
   def self.artist_count
     artist_hash = Hash.new(0)
     @@artists.each {|artist| artist_hash[artist] += 1}
+    artist_hash
   end
     
 end
